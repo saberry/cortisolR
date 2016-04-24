a63b8371cdade3315182a054034358062c1e66ec
Seth Berry <seth.berry@nd.edu>
2016-04-23 20:51:47 -0400

    Naming revisions

--- R/initialInspectionCortisol.R
+++ R/initialInspectionCortisol.R
@@ -5,13 +5,13 @@
 #' @usage initialInspectCortisol(dat, calibName, calibValue, nVar, drop)
 #' @param dat
 #' A data frame
-#' @param calibName
+#' @param calibVar
 #' The name of the calibration variable (usually STAND or A)
 #' @param calibValue
 #' The value for calibration (usually 0)
-#' @param
+#' @param cpmVar
 #' The name for the CPM variable (usually CPM)
-#' @param nVar
+#' @param idVar
 #' The ID variable; is often "N".
 #' @param drop
 #' Which participant to drop.
@@ -24,42 +24,57 @@
 #' @importFrom ggplot2 geom_smooth
 #' @importFrom ggplot2 %+%
 #' @importFrom ggplot2 aes
-initialInspectCortisol = function(dat, calibName, calibValue, cpmName,
-                          nVar = NULL, drop = NULL) {
 
-  if(calibName %in% names(dat) == FALSE) stop(paste(calibName, "is not in your data.", sep = " "))
+initialInspectCortisol = function(dat, calibVar, calibValue, cpmVar,
+                          idVar = NULL, drop = NULL) {
+
+  if(calibVar %in% names(dat) == FALSE) stop(paste(calibVar, "is not in your data.", sep = " "))
 
   if (is.null(drop)) {
-    initialDat = dat[dat[, calibName] %in% calibValue, ]
+    initialDat = dat[dat[, calibVar] %in% calibValue, ]
   } else {
-    initialDat = dat[dat[, calibName] %in% calibValue & corDat[, nVar] != drop, ]
-    if(nVar %in% names(dat) == FALSE) stop(paste(nVar, "is not in your data.", sep = " "))
+    initialDat = dat[dat[, calibVar] %in% calibValue & corDat[, idVar] != drop, ]
+    if(idVar %in% names(dat) == FALSE) stop(paste(idVar, "is not in your data.", sep = " "))
   }
 
-  cpmMean = mean(dat[[cpmName]], na.rm = TRUE)
+  cpmMean = mean(dat[[cpmVar]], na.rm = TRUE)
 
-  bindings = (dat[[cpmName]]/cpmMean) * 100
+  bindings = (dat[[cpmVar]]/cpmMean) * 100
 
   logVals = log((100 - bindings) / bindings)
 
-  logStd = log(dat[[calibName]])
+  logStd = log(dat[[calibVar]])
 
   plotDat = cbind(dat, bindings, logVals, logStd)
 
-  plotDat = plotDat
+  logStdMod = lm(logVals ~ logStd, data = plotDat[plotDat$logStd >= 0, ])
 
-  p1 = ggplot(plotDat, aes(logStd, plotDat[[calibName]])) +
+  logStdConstant = logStdMod$coefficients[[1]]
+
+  logStdCoef = logStdMod$coefficients[[2]]
+
+  logModSum = summary(logStdMod)
+
+  logModRSqr = logModSum$adj.r.squared
+
+  resultReturn = paste("Your R-square is", round(logModRSqr, 5))
+
+  p1 = ggplot(plotDat, aes(logStd, plotDat[[calibVar]])) +
     geom_smooth(span = .5) +
     geom_point() +
+    scale_y_continuous(name = "STAND") +
+    ggtitle(resultReturn) +
     ggtheme()
 
-  p2 = ggplot(plotDat, aes(logVals, plotDat[[calibName]])) +
+  p2 = ggplot(plotDat, aes(logVals, plotDat[[calibVar]])) +
     geom_point() +
+    scale_y_continuous(name = "STAND") +
     ggtheme()
 
-  p3 = ggplot(plotDat, aes(logVals, plotDat[[calibName]])) +
+  p3 = ggplot(plotDat, aes(logVals, plotDat[[calibVar]])) +
     geom_smooth(method = "lm") +
     geom_point() +
+    scale_y_continuous(name = "STAND") +
     ggtheme()
 
   p4 = ggplot(plotDat, aes(logVals, logStd)) +
@@ -73,16 +88,5 @@ initialInspectCortisol = function(dat, calibName, calibValue, cpmName,
 
   multiplot(p1, p2, p3, p4, p5)
 
-  logStdMod = lm(logVals ~ logStd, data = plotDat[plotDat$logStd >= 0, ])
-
-  logStdConstant = logStdMod$coefficients[[1]]
-
-  logStdCoef = logStdMod$coefficients[[2]]
-
-  logModSum = summary(logStdMod)
-
-  logModRSqr = logModSum$adj.r.squared
-
-  paste("Your R-square is", round(logModRSqr, 5))
-
+  return(plotDat)
 }
